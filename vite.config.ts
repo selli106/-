020f5b5fc0fb7c0b3b0c965876e0e496ede0532c
selli106 <49331083+selli106@users.noreply.github.com>
2025-10-07 13:35:09 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  // Change 'your-repository-name' to your actual repo name
+  base: '/BookshELF/', 
   plugins: [react()],
 })
